e034ae765edf0dfaeeff5702eba0ba79610a595b
simar-singh <simar.singh.r@gmail.com>
2021-10-07 21:39:39 -0400

    Added commenting
    Added comments to js files and index and tutoring files

--- header_and_footer.js
+++ header_and_footer.js
@@ -1,8 +1,12 @@
+// JQuery to get data json and generate header and footer info
 $.getJSON('data.json').done((data) => {
+  // Header generation
   $.each(data.header, (str) => {
+    // Add URL and text
     $("#" + str).attr("href", data.header[str].href).html(data.header[str].txt);
   })
 
+  // Add footer links with mailto
   $("#footer_1").html("President: <a href=\""+ data.footer.email1.mailto + "\">" + data.footer.email1.address + "</a>")
   $("#footer_2").html("GNH Mailing List: <a href=\"" + data.footer.email2.mailtow + "\">" + data.footer.email2.address +"</a>")
 });
